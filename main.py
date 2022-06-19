8e264d5832c60bcebdca29046a08632cd4272c1e
Leon Wang <j663wang@edu.uwaterloo.ca>
2022-06-19 00:53:18 -0400

    add running status and resource folder

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ import time
 from pygame.locals import *
 
 # dictionary of color and corresponding RGB value
+#   Key:
+#       color name for pure colors : "red" for pure red
+#       prefix d for dark colors   : "d_red" for dark red
 #   Example:
 #           Color_map["red"] -> (255,0,0)
 Color_map = {"red": (255, 0, 0),
              "gree": (0, 255, 0),
+             "d_green": (0, 133, 46),
              "blue": (0, 0, 255),
-             "black": (255, 255, 255),
-             "white": (0, 0, 0)}
+             "black": (0, 0, 0),
+             "white": (255, 255, 255)}
 
 
 # function that prints text at designated pos
@@ -33,21 +37,30 @@ def show_txt(window, txt="Hello", x=0, y=0):
 if __name__ == "__main__":
     pygame.init()
     board = pygame.display.set_mode((900, 900))
-    board.fill(Color_map["black"])
+    board.fill(Color_map["white"])
     # need to update the screen using flip or update
-    # flip: redraw
-    # update: update on existing
+    # flip: update the whole screen
+    # update: can update part of the screen or the whole screen
     pygame.display.flip()
 
     # flag for exiting event loop
-    running = True
+    # Possible value:
+    #               'R' : running
+    #               'E' : end
+    #               'I' : init(start a new game)
+    run_sts = 'I'
 
-    while running:
+    while run_sts != 'E':
         for event in pygame.event.get():
             if event.type == KEYDOWN:
+                # if hit esc key, exit
                 if event.key == K_ESCAPE:
-                    running = False
-                elif event.key == K_RETURN:
-                    pass
+                    run_sts = 'E'
 
+                elif event.key == K_RETURN:
+                    if run_sts == 'I':
+                        run_sts == 'R'
+            elif event.type == QUIT:
+                run_sts = 'E'
+    # remains: if the game end save the record to a text file
     time.sleep(4)
